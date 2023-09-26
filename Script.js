1d591b5dc6d200e03bb4b34bbbef4756d43e7206
Diogo Jose <124062577+Diogjos@users.noreply.github.com>
2023-09-26 16:43:32 -0300

    Update

---
+++ Script.js
@@ -0,0 +1,4 @@
+document.getElementById('timer').innerHTML = '25:00';
+document.getElementById('start_stop').innerHTML = 'Start';
+document.getElementById('ajuste_atividade').innerHTML = '25 min';
+document.getElementById('ajuste_pausa').innerHTML = '05 min';
