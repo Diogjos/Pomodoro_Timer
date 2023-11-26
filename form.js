9624b0d379273ea16d687922fe6c4f20789fdd7e
Diogo Jose <124062577+Diogjos@users.noreply.github.com>
2023-11-26 14:28:24 -0300

    release

---
+++ form.js
@@ -0,0 +1,5 @@
+let name = document.querySelector('.name');
+let mail = document.querySelector('.mail');
+let mensagem = document.querySelector('.mennsage');
+let btn = document.querySelector('.btn');
+
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
+var mysql = require('mysql');
 
 http.createServer(function (req, res) {
   var q = url.parse(req.url, true);
@@ -15,3 +16,24 @@ http.createServer(function (req, res) {
     return res.end();
   });
 }).listen(8080); 
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "1234",
+  database: "Usuario"
+});
+
+//con.connect(function(err) {
+  //if (err) throw err;
+  //console.log("Connected!");
+//});
+
+con.connect(function(err) {
+  if (err) throw err;
+    console.log("Connected!");
+  con.query("SELECT * FROM Mensagem", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
